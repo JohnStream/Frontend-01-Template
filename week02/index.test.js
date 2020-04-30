ba96f8967f77a8428b3387dc24916d71b735fe16
liulu <liulu@geetest.com>
2020-04-30 16:12:49 +0800

    feat(week02): 新增正则判断对象字面量case✔

--- week02/index.test.js
+++ week02/index.test.js
@@ -1,5 +1,74 @@
-const sum = require('./index');
+const verifyNumber = require('./index');
 
-test('adds 1 + 2 to equal 3', () => {
-  expect(sum(1, 2)).toBe(3);
+test('0 is NumericLiteral', () => {
+  expect(verifyNumber('0')).toBeTruthy();
+});
+
+test('01 is NumericLiteral', () => {
+  expect(verifyNumber('01')).toBeTruthy();
+});
+
+test('0.111111111 is NumericLiteral', () => {
+  expect(verifyNumber('0.111111111')).toBeTruthy();
+});
+
+test('0x10 is NumericLiteral', () => {
+  expect(verifyNumber('0x10')).toBeTruthy();
+});
+
+test('0X10 is NumericLiteral', () => {
+  expect(verifyNumber('0X10')).toBeTruthy();
+});
+
+test('1e2 is NumericLiteral', () => {
+  expect(verifyNumber('1e2')).toBeTruthy();
+});
+
+test('1E2 is NumericLiteral', () => {
+  expect(verifyNumber('1E2')).toBeTruthy();
+});
+
+test('0.1e2 is NumericLiteral', () => {
+  expect(verifyNumber('0.1e2')).toBeTruthy();
+});
+
+test('+10 is NumericLiteral', () => {
+  expect(verifyNumber('+10')).toBeTruthy();
+});
+
+test('-10 is NumericLiteral', () => {
+  expect(verifyNumber('-10')).toBeTruthy();
+});
+
+
+test('0b10000000 is NumericLiteral', () => {
+  expect(verifyNumber('0b10000000')).toBeTruthy();
+});
+
+test('0O13 is NumericLiteral', () => {
+  expect(verifyNumber('0O13')).toBeTruthy();
+});
+
+test('0o13 is NumericLiteral', () => {
+  expect(verifyNumber('0o13')).toBeTruthy();
+});
+
+test('0O is not NumericLiteral', () => {
+  expect(verifyNumber('0O')).toBeFalsy();
+});
+
+test('0o is not NumericLiteral', () => {
+  expect(verifyNumber('0o')).toBeFalsy();
+});
+
+test('0x is not NumericLiteral', () => {
+  expect(verifyNumber('0x')).toBeFalsy();
+});
+
+test('0X is not NumericLiteral', () => {
+  expect(verifyNumber('0X')).toBeFalsy();
+});
+
+test('xixi is not NumericLiteral', () => {
+  expect(verifyNumber('xixi')).toBeFalsy();
 });
--- week02/index.ts
+++ week02/index.ts
@@ -1,4 +1,7 @@
-function sum (a: number, b:number) {
-    return a + b
+
+function verifyNumber (str: string) {
+   let re = /((((0|[1-9]([0-9]+)?)\.([0-9]+)?([eE][+-]?[0-9]+)?|\.[0-9]+([eE][+-]?[0-9]+)?|(0|[1-9]([0-9]+)?)([eE][+-]?[0-9]+)?)|0[bB][01]+|0[oO][0-7]+|0[xX][0-9a-fA-F]+))/;
+   return re.test(str)
 }
-module.exports = sum
+
+module.exports = verifyNumber

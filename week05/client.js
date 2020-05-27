c91b7367574daf4cbd50fcf18c72baa646952599
liulu <1021283712@qq.com>
2020-05-27 16:25:24 +0800

    feat(week05): 还在补第五周的作业，感觉自己真棒呢😭

--- week05/client.js
+++ week05/client.js
@@ -1,10 +1,21 @@
 const net = require('net');
+// let str = 'Host: localhost:8080\r\n' + 'Accept: text/plain\r\n' + 'accept-encoding: gzip, deflate, br\r\n' + 'content-length: 3\r\n'
+let header = {
+    'Host': 'localhost:8080\r\n',
+    'Accept': 'accept-encoding: gzip, deflate, br\r\n',
+    'content-length': '3\r\n'
+}
+let test = Object.keys(header).map((key) => {
+    return key + ': ' + header[key]
+}).join('')
+console.log(test)
+console.log(typeof test)
 let client = net.createConnection({
     port: 8080
 }, () => {
     // TODO: 这里有LF和CRLF的区别
     console.log('服务器链接成功')
-    client.write('GET / HTTP/1.1\r\n' + 'Host: localhost:8080\r\n' + 'Accept: text/plain\r\n' + 'accept-encoding: gzip, deflate, br\r\n' + 'content-length: 3\r\n'+ '\r\n' + '123');
+    client.write('GET / HTTP/1.1\r\n' + test + '\r\n' + '123');
 });
 client.on('data', function (data) {
     // console.log(data.toString());
